63d3073130d2bee6db2710df16be3f75ded0b4ae
denis <denis@systemp>
2023-02-15 03:15:20 +0700

    New project

---
+++ demo_u.pas
@@ -0,0 +1,180 @@
+
+unit demo_u;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus,
+  ExtCtrls, ActnList, FileUtil, StrUtils, AsyncProcess, Process;
+
+type
+  TToolCommand = class
+  private
+    FCommand: string;
+  public
+    constructor CreateToolCommand(const ACommand: string);
+    property Command: string read FCommand write FCommand;
+  end;
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BTExecute: TButton;
+    BTSelectInput: TButton;
+    BTSelectOutput: TButton;
+    CBShell: TCheckBox;
+    CBRedirectErr: TCheckBox;
+    EDInput: TLabeledEdit;
+    EDOutput: TLabeledEdit;
+    EDCommand: TLabeledEdit;
+    LBOperations: TListBox;
+    MMMenu: TMainMenu;
+    MIUI: TMenuItem;
+    MIQuit: TMenuItem;
+    MMOutput: TMemo;
+    ODDialog: TOpenDialog;
+    SDDialog: TSaveDialog;
+    procedure AsyncProcess1ReadData(Sender: TObject);
+    procedure BTExecuteClick(Sender: TObject);
+    procedure BTSelectInputClick(Sender: TObject);
+    procedure BTSelectOutputClick(Sender: TObject);
+    procedure EDOutputChange(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure LBOperationsClick(Sender: TObject);
+    procedure MIQuitClick(Sender: TObject);
+  private
+    FCommandPattern: string;
+    procedure BuildCommandLine;
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses
+  DefaultTranslator;
+
+{$R *.lfm}
+
+resourcestring
+  rsWavToMp3Ffmpeg = 'Convert WAV to AAC q0.98 using Nero';
+  rsWmaToMp3Ffmpeg = 'Convert WAV to AAC q0.99 using Nero';
+
+constructor TToolCommand.CreateToolCommand(const ACommand: string);
+{ https://stackoverflow.com/a/16128750 }
+begin
+  inherited Create;
+  FCommand := ACommand;
+end;
+
+{ TForm1 }
+
+procedure TForm1.AsyncProcess1ReadData(Sender: TObject);
+{ https://forum.lazarus.freepascal.org/index.php/topic,53716.msg397941.html#msg397941 }
+var
+  LProcess: TAsyncProcess absolute Sender;
+  LStr: string = '';
+  LNum: integer;
+begin
+  LNum := LProcess.Output.NumBytesAvailable;
+  SetLength(LStr, LNum);
+  LNum := LProcess.Output.Read(LStr[1], LNum);
+  MMOutput.Append(Trim(LStr));
+end;
+
+procedure TForm1.BTExecuteClick(Sender: TObject);
+var
+  LProcess: TAsyncProcess;
+begin
+  LProcess := TAsyncProcess.Create(Self);
+  LProcess.OnReadData := @AsyncProcess1ReadData;
+  LProcess.Options := [poUsePipes, poNoConsole];
+  LProcess.ParseCmdLine(EDCommand.Text);
+  LProcess.Execute;
+end;
+
+procedure TForm1.BTSelectInputClick(Sender: TObject);
+begin
+  if ODDialog.Execute then
+    EDInput.Text := ODDialog.Filename;
+end;
+
+procedure TForm1.BTSelectOutputClick(Sender: TObject);
+begin
+  if SDDialog.Execute then
+    EDOutput.Text := SDDialog.Filename;
+end;
+
+procedure TForm1.EDOutputChange(Sender: TObject);
+begin
+  BuildCommandLine;
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+{ https://www.developpez.net/forums/d2089611/autres-langages/pascal/lazarus/ajuster-hauteur-d-fenetre-contenant-barre-menu/#post11610251 }
+var
+  LMenuHeight: integer;
+begin
+  LMenuHeight := Self.Height - Self.ClientHeight;
+  Self.Height := MMOutput.Top + MMOutput.Height + 8 + LMenuHeight;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+var
+  i: Integer;
+begin
+  for i := 0 to Pred(LBOperations.Items.Count) do
+    LBOperations.Items.Objects[i].Free;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FCommandPattern := EmptyStr;
+  LBOperations.Items.AddObject(rsWavToMp3Ffmpeg, TToolCommand.CreateToolCommand('nero -q 0.98 -lc -if "%s" -of "%s"'));
+  LBOperations.Items.AddObject(rsWmaToMp3Ffmpeg, TToolCommand.CreateToolCommand('nero -q 0.99 -lc -if "%s" -of "%s"'));
+end;
+
+procedure TForm1.LBOperationsClick(Sender: TObject);
+var
+  LToolCommand: TToolCommand;
+begin
+  if LBOperations.ItemIndex <> -1 then
+  begin
+    LToolCommand := TToolCommand(LBOperations.Items.Objects[LBOperations.ItemIndex]);
+    FCommandPattern := LToolCommand.Command;
+  end;
+end;
+
+procedure TForm1.MIQuitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.BuildCommandLine;
+begin
+  if (Length(FCommandPattern) > 0)
+  and (Length(EDInput.Text) > 0)
+  and (Length(EDOutput.Text) > 0) then
+  begin
+    EDCommand.Text := Format(FCommandPattern, [EDInput.Text, EDOutput.Text]);
+    if CBShell.Checked then
+      EDCommand.Text := Concat(
+        FindDefaultExecutablePath('sh'),
+        ' -c ''',
+        EDCommand.Text,
+        ' ',
+        IfThen(CBRedirectErr.Checked, '2>&1', ''),
+        ''''
+      );
+  end;
+end;
+
+end.
+
